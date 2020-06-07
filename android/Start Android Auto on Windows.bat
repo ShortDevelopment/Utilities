5e8ac143bd23879b182ca1fc01bf3a3b57181e37
Lukas Kurz <55882808+ShortDevelopment@users.noreply.github.com>
2020-06-07 21:12:13 +0200

    Add files via upload

---
+++ android/Start Android Auto on Windows.bat
@@ -0,0 +1,4 @@
+title Android Auto
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe" forward tcp:5277 tcp:5277
+"C:\Program Files (x86)\Android\android-sdk\extras\google\auto\desktop-head-unit.exe"
+pause
