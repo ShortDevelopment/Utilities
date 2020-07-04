ca56b0f1aed88a57323c44c6765c7f1d98ca2c4c
Lukas Kurz <55882808+ShortDevelopment@users.noreply.github.com>
2020-07-04 20:02:59 +0200

    Start UWP App and get ID
    Example input: *Minecraft*

---
+++ windows/GetAppxPackageStartID.ps1
@@ -0,0 +1,17 @@
+﻿# https://docs.microsoft.com/en-us/windows/configuration/find-the-application-user-model-id-of-an-installed-app
+cls
+$filter = Read-Host 'App-Filter'
+$installedapps = get-AppxPackage $filter
+
+$aumidList = @()
+foreach ($app in $installedapps)
+{
+    foreach ($id in (Get-AppxPackageManifest $app).package.applications.application.id)
+    {
+        $aumidList += $app.packagefamilyname + "!" + $id
+    }
+}
+
+$aumidList
+# start explorer.exe shell:appsFolder\$aumidList
+# Other options: https://docs.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-iapplicationactivationmanager-activateapplication
